f3938bdec8578d46a4fa6567f85c3c97b2396e6f
Baptiste025 <r0982291@ucll.be>
2025-11-18 19:34:28 +0100

    commit to pull not right script i think

--- scripts/retriever.py
+++ scripts/retriever.py
@@ -3,10 +3,15 @@ from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from get_triviaqa_huggingace import load_data
 
+# def structure_evidence(evidencefolder):
+
+
 def retriever(question, evidence):
-    docs = load_data()
+    data = load_data()
+    # evidence = structure_evidence(evidencefolder)
     splitter = RecursiveCharacterTextSplitter(chunk_size=512, chunk_overlap=30)
     chunked_docs = splitter.split_documents(docs)
+    # change docs to evidence 
 
     # this is the embeddings database
     db = FAISS.from_documents(chunked_docs, HuggingFaceEmbeddings(model_name='BAAI/bge-base-en-v1.5'))
