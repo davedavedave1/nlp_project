cd397102e9ca3c6999b49c48ea56bfd49a7543c1
David <david.leyendecker@gmx.de>
2025-11-18 19:44:59 +0100

    created evidence folder and worked on retriever

--- scripts/retriever.py
+++ scripts/retriever.py
@@ -1,18 +1,22 @@
-from langchain.vectorstores import FAISS
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from get_triviaqa_huggingace import load_data
+from langchain_community.vectorstores import FAISS
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from load_data import load_data
 
 def retriever(question, evidence):
     docs = load_data("../necessary_parts_triviaqa/evidence/wikipedia")
     splitter = RecursiveCharacterTextSplitter(chunk_size=512, chunk_overlap=30)
     chunked_docs = splitter.split_documents(docs)
-
-    # this is the embeddings database
-    db = FAISS.from_documents(chunked_docs, HuggingFaceEmbeddings(model_name='BAAI/bge-base-en-v1.5'))
-
-    retriever = db.as_retriever(
-        search_type="similarity",
-        search_kwargs={'k': 4})
     
-retriever()
+    # this is the embeddings database
+    db = FAISS.from_documents(
+        chunked_docs, 
+        HuggingFaceEmbeddings(model_name='BAAI/bge-base-en-v1.5')
+    )
+    
+    retriever_obj = db.as_retriever(
+        search_type="similarity",
+        search_kwargs={'k': 4}
+    )
+    
+    
