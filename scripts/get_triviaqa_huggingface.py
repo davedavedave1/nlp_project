1de0ffea904f7224af220d6ca99687e020f8b749
Baptiste025 <r0982291@ucll.be>
2025-11-18 19:17:52 +0100

    commit my changes before pulling blablabla

--- scripts/get_triviaqa_huggingface.py
+++ scripts/get_triviaqa_huggingface.py
@@ -1,18 +1,12 @@
-from datasets import get_dataset_config_names
 from datasets import load_dataset
 
-
-
-def main():
-    #domains = get_dataset_config_names('trivia_qa')
-    #print(domains)
-    ds = load_dataset('trivia_qa', name="rc.wikipedia")
-    simplified_ds = []
-    #for tripel in ds["validation"]
-        #simplified_ds
-    print(ds["validation"]["question"])
-    #print(ds["validation"][0]["question"])
-
+def load_data():
+    print("Loading dataset...")
+    ds = load_dataset('trivia_qa', name="rc.wikipedia", split="validation")    
+    return ds
 
 if __name__ == "__main__":
-    main()
+    # had to split up the function so we could call the loader function from the retriever
+    data = load_data()
+
+    print(data[0]["question"])
