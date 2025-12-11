444815163f7c409f6c6fee38fa07a693f78ffc59
johanka <jjakubove@gmail.com>
2025-12-11 01:06:36 +0100

    More small changes to colab

--- scripts/main_for_colab.py
+++ scripts/main_for_colab.py
@@ -1,5 +1,5 @@
 import embedder
-import test_with_triviaqa
+from test_with_triviaqa import test_with_triviaqa
 from pathlib import Path
 import subprocess
 
@@ -17,42 +17,22 @@ def run_create_partition():
 
 
 if __name__ == "__main__":
-    if not Path("necessary_evidence_triviaqa/wikipedia-train_new.json").exists():
+    if not Path("./necessary_parts_triviaqa/wikipedia-train_new.json").exists():
         run_create_partition()
 
     testfile = "./necessary_parts_triviaqa/wikipedia-development_new.json"
     prefix_path_to_db = "./databases/FAISS-DB_embeddingModel~paraphrase-MiniLM-L3-v2_chunkSize~"
-    suffix_path_to_db = "_chunkOverlap~30"
+    suffix_path_to_db = "_chunkOverlap~30_WITH_METADATA"
 
     chunks = [256, 512, 1024, 2048, 4096, 8192]
-
     for chunk in chunks:
         if not Path(prefix_path_to_db + str(chunk) + suffix_path_to_db).exists():
+            print("Starting embedding chunk:",chunk)
             embedder.embedder(chunk,30, False)
 
-    res = []
-    res.append(
-        test_with_triviaqa(testfile, prefix_path_to_db + "256" + suffix_path_to_db, 64, 32, True, debug=False) + " \n")
-    with open("resultswithrerankerbackup1.txt", "w") as t:
-        t.writelines(res)
-    res.append(
-        test_with_triviaqa(testfile, prefix_path_to_db + "512" + suffix_path_to_db, 32, 16, True, debug=False) + " \n")
-    with open("resultswithrerankerbackup2.txt", "w") as t:
-        t.writelines(res)
-    res.append(
-        test_with_triviaqa(testfile, prefix_path_to_db + "1024" + suffix_path_to_db, 16, 8, True, debug=False) + " \n")
-    with open("resultswithrerankerbackup3.txt", "w") as t:
-        t.writelines(res)
 
-    res.append(
-        test_with_triviaqa(testfile, prefix_path_to_db + "2048" + suffix_path_to_db, 8, 4, True, debug=False) + " \n")
-    with open("resultswithrerankerbackup4.txt", "w") as t:
-        t.writelines(res)
-    res.append(
-        test_with_triviaqa(testfile, prefix_path_to_db + "4096" + suffix_path_to_db, 4, 2, True, debug=False) + " \n")
-    with open("resultswithrerankerbackup5.txt", "w") as t:
-        t.writelines(res)
-    res.append(
-        test_with_triviaqa(testfile, prefix_path_to_db + "8192" + suffix_path_to_db, 2, 1, True, debug=False) + " \n")
-    with open("resultswithrerankerbackup6.txt", "w") as t:
-        t.writelines(res)
+    for chunk,n in [(256, 64), (512, 32), (1024, 16), (2048, 8), (4096, 4), (8192, 2)]:
+        res= test_with_triviaqa(testfile, prefix_path_to_db + str(chunk) + suffix_path_to_db, n, n//2, True, debug=False) + " \n"
+        with open("/content/d/MyDrive/Colab Notebooks/nlp_project_files/results_colab_round1.txt", "a") as t:
+            t.writelines(res)
+
